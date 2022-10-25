a161e33a343e2e1492ed9d9e49942c9ce7183e3a
dangermyong <66460964+dangermyong@users.noreply.github.com>
2022-10-25 13:18:44 +0900

    final

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvDrjai-pBaRBBiiYBxQSMvb7NsPZy5RA",
-  authDomain: "jeildc-3a611.firebaseapp.com",
-  projectId: "jeildc-3a611",
-  storageBucket: "jeildc-3a611.appspot.com",
-  messagingSenderId: "1028180370116",
-  appId: "1:1028180370116:web:9398ffe85ce897534c9f91"
+  apiKey: "AIzaSyAEG2oPobv02jKbTkA-VuYXbdlXCKRNKC8",
+  authDomain: "jeildc-da954.firebaseapp.com",
+  projectId: "jeildc-da954",
+  storageBucket: "jeildc-da954.appspot.com",
+  messagingSenderId: "230734112603",
+  appId: "1:230734112603:web:4317a4a4ae47c023c8dc7e"
 }
 
 const firebaseApp = initializeApp(firebaseConfig)
